d4d8a05fb9bca8f8b6a6dfa2f05638c33ee70923
Mishon <nafis4627@gmail.com>
2021-04-14 23:52:43 +0600

    overloaded add funtion added

--- c.cpp
+++ c.cpp
@@ -5,8 +5,13 @@ void add(int a,int b,int c){
     cout<<a+b+c<<endl;
 }  
 
+int add(double a,double b){
+    return a+b;
+}
+
 int main(){
     int a,b,c;
     cin>>a>>b>>c;
     add(a,b,c);
+    cout<<add(a,b)<<endl;
 }
